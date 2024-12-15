3ce367bb966039812efca62e1ff667b260362b26
gaesigua <67262987+gaesigua@users.noreply.github.com>
2024-12-15 23:57:29 +0200

    dec15

---
+++ src/learnJava/LearnToCreateBookmarks.java
@@ -0,0 +1,19 @@
+package learnJava;
+
+public class LearnToCreateBookmarks {
+	public static void main(String[] args) {
+//		HTML Links - Create Bookmarks
+//		HTML links can be used to create bookmarks, so that readers can jump to specific parts of a web page.
+//
+//		Create a Bookmark in HTML
+//		Bookmarks can be useful if a web page is very long.
+//
+//		To create a bookmark - first create the bookmark, then add a link to it.
+//
+//		When the link is clicked, the page will scroll down or up to the location with the bookmark.
+//
+//		Example
+//		First, use the id attribute to create a bookmark:
+//	}
+//
+//}
